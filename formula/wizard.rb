f282483ef1ef6331d78dbaf995509effc8da7707
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2022-01-24 10:54:55 +0100

    Add CI-Wizard formula

---
+++ formula/wizard.rb
@@ -0,0 +1,19 @@
+require "formula"
+require_relative "../lib/private_strategy"
+
+class Dor < Formula
+  desc "DOR⚡️ Flutter CI-Wizard"
+  homepage "https://github.com/droidsonroids/flutter-ci-wizard"
+  url "https://github.com/DroidsOnRoids/flutter-ci-wizard/releases/download/0.1.0/wizard.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "d3c0c3b1dae71f1dcd4c16e5b465f8d93c27d26bb43f2618621ab5308755cf81"
+  head "https://github.com/DroidsOnRoids/flutter-ci-wizard.git"
+
+  def install
+    bin.install "wizard"
+  end
+
+  # Homebrew requires tests.
+  test do
+    assert_match "", ""
+  end
+end
