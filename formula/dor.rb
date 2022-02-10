55d141ca628dc8aa23eb27b851f206e03b9f2916
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2022-02-10 14:38:52 +0100

    Update SHA

--- formula/dor.rb
+++ formula/dor.rb
@@ -5,7 +5,7 @@ class Dor < Formula
   desc "DOR⚡️ Flutter Command Line Interface"
   homepage "https://github.com/droidsonroids/dor-flutter-cli"
   url "https://github.com/DroidsOnRoids/dor-flutter-cli/releases/download/0.2.4/dor.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "b5924d3e0d526c404c4004b3de43597ed07e4f5da988a36fbfbf8bc8e8bdcd0e"
+  sha256 "acdc68dc020a49cd540eb9633ffe2f3a54337875f818955e99066d61575ab4da"
   head "https://github.com/DroidsOnRoids/dor-flutter-cli.git"
 
   def install
