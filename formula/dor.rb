a3c4d67b1ae6a9a53200bac4811f1b74eefc6411
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2022-04-25 11:38:00 +0200

    Release DOR CLI 0.3.1

--- formula/dor.rb
+++ formula/dor.rb
@@ -4,8 +4,8 @@ require_relative "../lib/private_strategy"
 class Dor < Formula
   desc "DOR⚡️ Flutter Command Line Interface"
   homepage "https://github.com/droidsonroids/dor-flutter-cli"
-  url "https://github.com/DroidsOnRoids/dor-flutter-cli/releases/download/0.3.0/dor.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "79190a3a8cda4634ea34525fdf1f57ac94efc54237e6f684757650bc4d21a97f"
+  url "https://github.com/DroidsOnRoids/dor-flutter-cli/releases/download/0.3.1/dor.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "8a28f6849e6a26b86e02e5205310fb53f82bf6084c702d5f1690c30b39b614ac"
   head "https://github.com/DroidsOnRoids/dor-flutter-cli.git"
 
   def install
