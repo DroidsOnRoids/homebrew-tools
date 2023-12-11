d11ce072c7d671df969c2f3653845bedc49793e4
J. Doe (https://devcenter.bitrise.io/builds/setting-your-git-credentials-on-build-machines/) <please-set-your-email@bitrise.io>
2023-12-11 11:39:37 +0000

    Release DOR CLI 0.7.5

--- formula/dor.rb
+++ formula/dor.rb
@@ -4,8 +4,8 @@ require_relative "../lib/private_strategy"
 class Dor < Formula
   desc "DOR⚡️ Flutter Command Line Interface"
   homepage "https://github.com/droidsonroids/dor-flutter-cli"
-  url "https://github.com/DroidsOnRoids/dor-flutter-cli/releases/download/0.7.4/dor.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "9c37664c3595d1c8fd7c667db12289643090dbfb9d300fbb995678470d21ca4e"
+  url "https://github.com/DroidsOnRoids/dor-flutter-cli/releases/download/0.7.5/dor.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "c02871c51eec21c99baf9f182586347aee9a2b98cb123e5fb918a26268b72f18"
   head "https://github.com/DroidsOnRoids/dor-flutter-cli.git"
 
   def install
