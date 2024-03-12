bda12e0aeb8f47e70fab0f036bc7de1a14aaa6cd
Alexander Stolar <alexander.stolar@yahoo.com>
2024-03-12 10:51:13 +0100

    Updated sha256 for next version of archive in 0.7.7 in dor.rb

--- formula/dor.rb
+++ formula/dor.rb
@@ -5,7 +5,7 @@ class Dor < Formula
   desc "DOR⚡️ Flutter Command Line Interface"
   homepage "https://github.com/droidsonroids/dor-flutter-cli"
   url "https://github.com/DroidsOnRoids/dor-flutter-cli/releases/download/0.7.7/dor.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "9305229b70b5aef1c0361e2f71774c45c40516e292ee984263b39a3b23d0afa6"
+  sha256 "7a8737f51accd73a22937792141954c599874044b485578586f720ccc4ba5cdd"
   head "https://github.com/DroidsOnRoids/dor-flutter-cli.git"
 
   def install
